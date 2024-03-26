95bb7bf7ef1bb79167498b383674195cd3857bc4
Mikhail Artemov <babanlive@gmail.com>
2024-03-26 23:09:59 +0700

    add context in view

--- women/views.py
+++ women/views.py
@@ -3,19 +3,42 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
-def index(request):
-    return render(request, 'women/index.html')
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+def index(request): # HttpRequest
+
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
+
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1> <p>id: {cat_id}</p>")
+
 
 def categories_by_slug(request, cat_slug):
     if request.POST:
         print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
 
 def archive(request, year):
     if year > 2023:
