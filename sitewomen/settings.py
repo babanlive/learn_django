d4df3c53d871b1ce01e029fbe87e434e8bee9044
Mikhail Artemov <babanlive@gmail.com>
2024-04-08 12:58:02 +0700

    add templatetags

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "women.apps.WomenConfig",
-    "django-extensions",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
