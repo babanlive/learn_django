67b4f133d5564b75982437be54a5744688b7dc1d
Mikhail Artemov <babanlive@gmail.com>
2024-04-20 20:05:06 +0700

    edit admin.py

--- women/admin.py
+++ women/admin.py
@@ -1,15 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from .models import Category, Women
 
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "time_create", "is_published", "cat")
-    list_display_links = ("id", "title")
+    list_display = ("title", "time_create", "is_published", "cat", "brief_info")
+    list_display_links = ("title",)
     ordering = ["-time_create"]
     list_editable = ("is_published",)
     list_per_page = 5
+    actions = ("set_published", "set_draft")
+
+    @admin.display(description="краткое описание")
+    def brief_info(self, women: Women):
+        return f"{len(women.content)} символов"
+    
+    @admin.action(description="Опубликовать")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} статей")
+
+
+    @admin.action(description="Перевести в черновик")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"{count} статей переведены в черновик", messages.WARNING)
 
 
 @admin.register(Category)
