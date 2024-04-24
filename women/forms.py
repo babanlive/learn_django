3720a3fd064e9331a95793b6e1f47a3707ff2bae
Mikhail Artemov <babanlive@gmail.com>
2024-04-24 12:10:22 +0700

    edit AddPostForm - creating Forms associated with models

--- women/forms.py
+++ women/forms.py
@@ -1,37 +1,26 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.deconstruct import deconstructible
 
+from .models import Women
 
-from .models import Category, Husband
-
-
-@deconstructible
-class RussianValidator:
-    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
-    code = 'russian'
-
-    def __init__(self, message=None):
-        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
-
-    def __call__(self, value, *args, **kwargs):
-        if not (set(value) <= set(self.ALLOWED_CHARS)):
-            raise ValidationError(self.message, code=self.code)
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), max_length=2048, required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    class Meta:
+        model = Women
+        fields = ["title", "slug", "content", "is_published", "cat", "husband"]
+        lables = {"title": "Заголовок", "content": "Содержание"}
+        widget = {
+            "title": forms.TextInput(attrs={"class": "form-input"}),
+            "content": forms.Textarea(attrs={"cols": 50, "rows": 7}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["cat"].empty_label = "Категория не выбрана"
 
     def clean_title(self):
-        title = self.cleaned_data['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
-
-        if not (set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError("Должны присутствовать только русские символы, дефис и пробел.")
+        title = self.cleaned_data["title"]
+        if len(title) > 255:
+            raise ValidationError("Длина превышает 255 символов")
 
         return title
-    
