0a94c2816220a55b6132b8bfe7cbdef22880b727
Mikhail Artemov <babanlive@gmail.com>
2024-04-24 10:19:58 +0700

    add validators for AddPostForms

--- women/forms.py
+++ women/forms.py
@@ -1,7 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 
-from .models import Category, Husband, Women
 
+from .models import Category, Husband
+
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
@@ -10,3 +25,13 @@ class AddPostForm(forms.Form):
     is_published = forms.BooleanField(required=False)
     cat = forms.ModelChoiceField(queryset=Category.objects.all())
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+
+        if not (set(title) <= set(ALLOWED_CHARS)):
+            raise ValidationError("Должны присутствовать только русские символы, дефис и пробел.")
+
+        return title
+    
