b53f1dfe0ff3e44e3cef968fa9e390bcdc1d5066
Mikhail Artemov <babanlive@gmail.com>
2024-05-08 10:10:08 +0700

    edit login form - repalce def login/logout on class UserLogin(LoginView)

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -6,25 +7,33 @@ from django.urls import reverse
 from .forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == "POST":
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            clean_data = form.cleaned_data
-            user = authenticate(
-                request,
-                username=clean_data["username"],
-                password=clean_data["password"],
-            )
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse("home"))
-    else:
-        form = LoginUserForm()
-
-    return render(request, "users/login.html", {"form": form})
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "users/login.html"
+    extra_context = {"title": "Вход"}
 
 
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("users:login"))
+
+
+# def login_user(request):
+#     if request.method == "POST":
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             clean_data = form.cleaned_data
+#             user = authenticate(
+#                 request,
+#                 username=clean_data["username"],
+#                 password=clean_data["password"],
+#             )
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse("home"))
+#     else:
+#         form = LoginUserForm()
+
+#     return render(request, "users/login.html", {"form": form})
+
+
+# def logout_user(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse("users:login"))
