a91fdacfacbb0675b70316a0363f4ac51eb13a44
Mikhail Artemov <babanlive@gmail.com>
2024-05-16 22:30:42 +0700

    added tests for home and add_page

--- women/tests.py
+++ women/tests.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
 
 
-# Create your tests here.
 class GetPagesTestCase(TestCase):
     def setUp(self):
         "Инициализация перед выполнением каждого теста"
@@ -11,15 +11,17 @@ class GetPagesTestCase(TestCase):
     def test_mainpage(self):
         path = reverse('home')
         response = self.client.get(path)
-        print(dir(response))
-        self.assertEqual(response.status_code, HttpResponse.status_code)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertIn('women/index.html', response.template_name)
         self.assertTemplateUsed(response, 'women/index.html')
+        self.assertEqual(response.context_data['title'], "Главная страница")
 
-    def test_case_1(self):
-        pass
-
-    def test_case_2(self):
-        pass
+    def test_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
 
     def tearDown(self):
         "Действия после выполнения каждого теста"
