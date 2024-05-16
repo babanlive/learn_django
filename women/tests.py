babe2aa58278231fdbf3df84d6d208285b61a0a1
Mikhail Artemov <babanlive@gmail.com>
2024-05-17 00:24:27 +0700

    1)create fixtures tables db for using tests 2) added 2 tests in GetPagesTestCase

--- women/tests.py
+++ women/tests.py
@@ -3,8 +3,17 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from .models import Women
+
 
 class GetPagesTestCase(TestCase):
+    fixtures = [
+        'women_women.json',
+        'women_category.json',
+        'women_husband.json',
+        'women_tagpost.json',
+    ]
+
     def setUp(self):
         "Инициализация перед выполнением каждого теста"
 
@@ -14,7 +23,7 @@ class GetPagesTestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         # self.assertIn('women/index.html', response.template_name)
         self.assertTemplateUsed(response, 'women/index.html')
-        self.assertEqual(response.context_data['title'], "Главная страница")
+        self.assertEqual(response.context_data['title'], 'Главная страница')
 
     def test_redirect_addpage(self):
         path = reverse('add_page')
@@ -22,6 +31,32 @@ class GetPagesTestCase(TestCase):
         response = self.client.get(path)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertRedirects(response, redirect_uri)
+
+    def test_data_mainpage(self):
+        post_list = Women.published.select_related('cat')
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(len(response.context_data['posts']), 5)
+        self.assertQuerysetEqual(response.context_data['posts'], post_list[:5])
+
+    def test_paginated_mainpage(self):
+        path = reverse('home')
+        page = 2
+        paginate = 5
+        response = self.client.get(path + f'?page={page}')
+        post_lst = Women.published.select_related('cat')
+        second_page_posts = post_lst[(paginate * (page - 1)) : (paginate * page)]
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(len(response.context_data['posts']), 5)
+        self.assertQuerysetEqual(response.context_data['posts'], second_page_posts)
+
+    def test_show_post(self):
+        post = Women.published.get(pk=3)
+        path = reverse('post', kwargs={'post_slug': post.slug})
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['post'].content, post.content)
 
     def tearDown(self):
         "Действия после выполнения каждого теста"
