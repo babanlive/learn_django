8b210b538d4764b47b333ebe52ce329c6adf1d6d
Mikhail Artemov <babanlive@gmail.com>
2024-05-16 19:51:51 +0700

    updated style.css for women app

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -2,8 +2,10 @@ from django import template
 from django.db.models import Count
 
 import women.views as views
+
 from women.models import Category, TagPost
 from women.utils import menu
+
 
 register = template.Library()
 
@@ -15,10 +17,10 @@ def get_menu():
 
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
+    cats = Category.objects.annotate(total=Count('posts')).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('women/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
+    return {'tags': TagPost.objects.annotate(total=Count('tags')).filter(total__gt=0)}
